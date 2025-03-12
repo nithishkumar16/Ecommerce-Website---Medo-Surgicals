efc7b0d2fc4d5201a85b4acc4190fb79166b265b
nithish <nithish6@gmail.com>
2025-03-12 12:05:31 -0700

    included assets folder

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,107 @@
+import medologo from './medologo.jpg'
+
+
+import logo from './logo.png'
+import hero_img from './hero_img.png'
+import hero_img2 from './hero2.jpg'
+import cart_icon from './cart_icon.png'
+import bin_icon from './bin_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import exchange_icon from './exchange_icon.png'
+import profile_icon from './profile_icon.png'
+import quality_icon from './quality_icon.png'
+import search_icon from './search_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+import star_icon from './star_icon.png'
+import support_img from './support_img.png'
+import menu_icon from './menu_icon.png'
+import about_img from './about_img.png'
+import contact_img from './contact_img.png'
+import razorpay_logo from './razorpay_logo.png'
+import stripe_logo from './stripe_logo.png'
+import cross_icon from './cross_icon.png'
+import delivery from './delivery.png'
+import about from './about.jpg'
+
+export const assets = {
+    about,
+    delivery,
+    medologo,
+    logo,
+    hero_img,
+    hero_img2,
+    cart_icon,
+    dropdown_icon,
+    exchange_icon,
+    profile_icon,
+    quality_icon,
+    search_icon,
+    star_dull_icon,
+    star_icon,
+    bin_icon,
+    support_img,
+    menu_icon,
+    about_img,
+    contact_img,
+    razorpay_logo,
+    stripe_logo,
+    cross_icon
+}
+
+export const categories = [
+    {
+        name: 'IV Solutions & Drugs',
+        description: 'All large‐volume parenterals, injectables, or irrigation solutions',
+        count: '300+ Products'
+    },
+    {
+        name: 'Sutures & Staplers',
+        description: 'Absorbable, nonabsorbable, staplers, etc.',
+        count: '150+ Products'
+    },
+    {
+        name: 'Wound Care & Dressings',
+        description: 'Foams, hydrocolloids, gauze pads, bandages, tapes',
+        count: '200+ Products'
+    },
+    {
+        name: 'Gloves & Hand-Hygiene',
+        description: 'Sterile surgical gloves, exam gloves, hand disinfectants, scrubs',
+        count: '100+ Products'
+    },
+    {
+        name: 'Respiratory & Airway',
+        description: 'Endotracheal tubes, tracheostomy tubes, circuits, masks, filters, etc.',
+        count: '180+ Products'
+    },
+    {
+        name: 'Catheters & Urology',
+        description: 'Foley catheters, ureteric catheters, stents, drainage bags',
+        count: '120+ Products'
+    },
+    {
+        name: 'Orthotics & Supports',
+        description: 'Lumbar belts, abdominal corsets, knee braces, cervical collars',
+        count: '150+ Products'
+    },
+    {
+        name: 'Diagnostic Strips & Sampling',
+        description: 'Glucometer test strips, lancets, ABG syringes, vacutainers',
+        count: '80+ Products'
+    },
+    {
+        name: 'General Disposables & Packs',
+        description: 'Drapes, gowns, procedure kits, suction devices, syringes, needles',
+        count: '400+ Products'
+    },
+    {
+        name: 'Ostomy & Stoma Care',
+        description: 'One‐piece/Two‐piece ostomy systems, stoma powders/pastes, belts',
+        count: '90+ Products'
+    },
+    {
+        name: 'Sterilization & Disinfection',
+        description: 'Chemical indicators, wraps, disinfectant solutions, sterilant cartridges',
+        count: '120+ Products'
+    }
+];
